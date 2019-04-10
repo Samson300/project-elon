00f83b9241e4a71472585852e032ee6acee50715
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-04-10 10:49:50 -0400

    defines structures

---
+++ models/conn.js
@@ -0,0 +1,15 @@
+const pgp = require('pg-promise')({
+    query: e => {
+        console.log('QUERY: ', e.query);
+    }
+});
+
+const options = {
+    host: 'localhost',
+    // Need to add database name
+    database: ''
+}
+
+const db = pgp(options);
+
+module.exports = db;
